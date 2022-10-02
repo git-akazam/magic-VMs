4470f35acefd4a8bd81a08a66119752ad46932bf
kitkat <you@example.com>
2022-10-02 17:16:24 +0300

    npm

--- akazamnpm.sh
+++ akazamnpm.sh
@@ -5,17 +5,17 @@
 
 
 
-echo "removeneed" && apt install npm & sleep 1 &
+echo "removeneed" && apt install npm -y & sleep 1 &
 wait $PID
-echo "Process 0 ended at time $(date +%T) with exit status $?"
+echo "Process 1 ended at time $(date +%T) with exit status $?"
 wait
 
 echo "removeneed" && npm install -g npm & sleep 1 &
 wait $PID
-echo "Process 0 ended at time $(date +%T) with exit status $?"
+echo "Process 2 ended at time $(date +%T) with exit status $?"
 wait
 
 echo "removeneed" &&npm install -g node& sleep 1 &
 wait $PID
-echo "Process 0 ended at time $(date +%T) with exit status $?"
+echo "Process 3 ended at time $(date +%T) with exit status $?"
 wait
