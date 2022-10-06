f028488ff0dc7ca60e6c7af4ef5b5204089e1816
kitkat <you@example.com>
2022-10-06 09:58:48 -0400

    small upgrade

--- aka33.sh
+++ aka33.sh
@@ -4,7 +4,7 @@
 
 
 PID=$!
-echo "xrdpstop" && sudo service xrdp start & sleep 1 &
+echo "xrdpstart" && sudo service xrdp start & sleep 1 &
 wait $!
 echo "Process 10 ended at time $(date +%T) with exit status $?"
 wait
