f3748c6f17fd1757ac1d5a0704af1e7802159bf9
kitkatty <34012208+kitkatty@users.noreply.github.com>
2023-06-05 00:33:22 -0500

    Update akanodescons.sh

--- akanodescons.sh
+++ akanodescons.sh
@@ -4,7 +4,7 @@
 
 
 PID=$!
-echo "consensusclient" && cd ethereum && cd consensus && screen -dm -S "consclient" ./prysm.sh beacon-chain --execution-endpoint=http://localhost:8551 --jwt-secret=/root/ethereum/consensus/prysm/jwt.hex --suggested-fee-recipient=0x4127dC53c9B300ecc75E34B17C649792039fc22e --checkpoint-sync-url=https://beaconstate.ethstaker.cc --genesis-beacon-api-url=https://beaconstate.ethstaker.cc --accept-terms-of-use & sleep 1 &
+echo "consensusclient" && cd ethereum && cd consensus && cd prysm && screen -dm -S "consclient" ./prysm.sh beacon-chain --execution-endpoint=http://localhost:8551 --jwt-secret=/root/ethereum/consensus/prysm/jwt.hex --suggested-fee-recipient=0x4127dC53c9B300ecc75E34B17C649792039fc22e --checkpoint-sync-url=https://beaconstate.ethstaker.cc --genesis-beacon-api-url=https://beaconstate.ethstaker.cc --accept-terms-of-use & sleep 1 &
 wait $!
 echo "Process 1 ended at time $(date +%T) with exit status $?"
 wait
