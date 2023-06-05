be5b6a20ac45d4a516933aa8b05dc554dd8f760f
kitkatty <34012208+kitkatty@users.noreply.github.com>
2023-06-05 00:24:57 -0500

    Update akanodesstart.sh

--- akanodesstart.sh
+++ akanodesstart.sh
@@ -6,4 +6,14 @@ wait $PID
 echo "Process 0 ended at time $(date +%T) with exit status $?"
 wait
 
+echo "sudoeth" && cp akanodesexec.sh /root/akanodesexec.sh & sleep 1 &
+wait $PID
+echo "Process 0 ended at time $(date +%T) with exit status $?"
+wait
+
+echo "sudoeth" && cp akanodescons.sh /root/akanodescons.sh & sleep 1 &
+wait $PID
+echo "Process 0 ended at time $(date +%T) with exit status $?"
+wait
+
 
